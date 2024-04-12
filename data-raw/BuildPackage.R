b678cbdf0d8750541a48d8ebced1d86e2b2a4931
bmcgaughey1 <bmcgaughey1@gmail.com>
2024-04-12 10:13:41 -0400

    Removing dependency on rgdal and rgeos

--- data-raw/BuildPackage.R
+++ data-raw/BuildPackage.R
@@ -3,10 +3,8 @@
 #
 
 # packages
-library(rgdal)
 library(sf)
 library(raster)
-library(rgeos)
 
 # mapview is the easiest way I have found to display geospatial data layers!!
 # used mostly for verifying outputs
